95e522814d6ae9ef367326fd4a687b086dd26dd1
Ilya Betyaev <betyaev.ilya2004@gmail.com>
2024-11-30 23:21:16 +0300

    Django Base

--- news_bot/bot/main_bot.py
+++ news_bot/bot/main_bot.py
@@ -1,11 +1,9 @@
 import os
-from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode 
 
-load_dotenv()   
 API_TOKEN=os.getenv('TOKEN')
 
 
